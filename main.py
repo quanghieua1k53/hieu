5232705798019cee1e041b21b1e517ae86dc2628
quanghieua1k53 <73240250+quanghieua1k53@users.noreply.github.com>
2021-12-10 12:16:44 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,271 +1,52 @@
-import numpy as np
+# This file will use to score your implementations.
+# You should not change this file
+
+import os
+import pandas as pd
+import time
+import sys
 import cv2
-import math
-import pytesseract
 
-#variable
-thetaErr = math.pi / 6
-debug = False
-rhoErr = 20
-report = False
+from simple_ocr import HoadonOCR
 
+if __name__ == "__main__":
 
-#Read File
+    input_folder = sys.argv[1]
+    label_file = sys.argv[2]
 
-img = cv2.imread('D:\Document\XLA\simpleOCR\sampledata\img2.jpeg',1)
+    df_labels = pd.read_csv(label_file)
+    img_name = df_labels['img_name'].values.tolist()
+    img_labels = df_labels['label'].values.tolist()
+    img_to_label = dict([(img_name[i], img_labels[i]) for i in range(len(img_name))])
 
-# Tesseract-ocr source
-pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+    start_time = time.time()
+    model = HoadonOCR()
+    init_time = time.time() - start_time
+    print("Run time in: %.2f s" % init_time)
 
-def getDistance(a,b):
-    return np.linalg.norm(a - b)
+    list_files = os.listdir(input_folder)
+    print("Total test images: ", len(list_files))
+    fail_process = 0
+    cnt_predict = 0
 
-def getOrderPoints(points):
-    '''
-    Divide points into 2 part: left and right
-    sort the points based on x-coordinates
-    '''
-    sortArgX = np.argsort(points[:,0])
-    left = np.array([points[x] for x in sortArgX[0:2]])
-    right = np.array([points[x] for x in sortArgX[2:4]])
-    #point with bigger y is bottomLeft and vice versa
-    bottomLeft = left[np.argmax(left[:,1])]
-    topLeft = left[np.argmin(left[:,1])]
-    #point that is farther from the topLeft is bottomRight
-    if getDistance(topLeft, right[0]) > getDistance(topLeft, right[1]):
-        bottomRight = right[0]
-        topRight = right[1]
-    else:
-        bottomRight = right[1]
-        topRight = right[0]
-    return (topLeft, topRight, bottomRight, bottomLeft)
-
-def getParallel(line, point):
-    rho, theta = line
-    return getRho(theta)(point), theta
-
-#Geting `rho` of a line that goes through `point` with angle `theta`
-def getRho(theta):
-    def result(point):
-        #point is of the form (x, y)
-        return point[0] * math.cos(theta) + point[1] * math.sin(theta)
-    return result
-
-def getIntersection(line1, line2):
-    #lines are of the form (rho, theta)
-    if debug:
-        print(line1)
-        print(line2)
-    rho1, theta1 = line1
-    rho2, theta2 = line2
-    A = np.array([[math.cos(theta1), math.sin(theta1)],
-                [math.cos(theta2), math.sin(theta2)]])
-    B = np.array([rho1, rho2])
-    if debug:
-        print(A)
-        print(B)
-    #return form: np.array([x, y]), may raise exception
-    result = np.linalg.solve(A, B)
-    return result
-
-def getBoundaryIntersections(line, img):
-    rho = line[0]
-    theta = line[1]
-    if theta >= math.pi / 2:
-        newTheta = theta - math.pi / 2
-    else:
-        newTheta = theta + math.pi / 2
-    height = img.shape[0]
-    width = img.shape[1]
-    leftBound = (0, 0)
-    rightBound = (width, 0)
-    topBound = (0, math.pi / 2)
-    bottomBound = (height, math.pi / 2)
-    bounds = (leftBound, rightBound, topBound, bottomBound)
-    intersections = list()
-    for bound in bounds:
+    start_time = time.time()
+    for filename in list_files:
+        img = cv2.imread(os.path.join(input_folder, filename))
+        print(img.shape)
         try:
-            intersection = getIntersection(line, bound)
-        except np.linalg.linalg.LinAlgError:
-            continue
-        else:
-            intersections.append(intersection)
-    rhos = [getRho(newTheta)(point) for point in intersections]
-    intersections = np.array(intersections)
-    numPoints = len(intersections)
-    if numPoints == 4:
-        return list(intersections[np.argsort(rhos)][1:3])
-    elif numPoints == 2:
-        return list(intersections)
-    else:
-        raise Exception("Error in GetBoundaryIntersections: Not enough points")
+            label = model.find_label(img)
+        except:
+            label = -1
 
-def checkSimilarRho(line, correctLine, img, rhoErr = 20):
-    rho, theta = line
-    intersections = getBoundaryIntersections(correctLine, img)
-    rhos = [getRho(theta)(intersection) for intersection in intersections]
-    if rho < max(rhos) + rhoErr and rho > min(rhos) - rhoErr:
-        return True
-    return False
+        if img_to_label[filename] == label:
+            cnt_predict += 1
+        elif label == -1:
+            print(filename)
+            fail_process += 1
+        print(label, filename)
+    run_time = time.time() - start_time
 
-def checkSimilarAngle(theta1, theta2):
-    if theta1 <= thetaErr / 2 and theta2 >= math.pi - thetaErr / 2:
-        return True  # , (theta1, theta2)
-    elif theta2 <= thetaErr / 2 and theta1 >= math.pi - thetaErr / 2:
-        return True  # , (theta2, theta1)
-    elif abs(theta1 - theta2) < thetaErr:
-        return True  # , (theta1, theta2)
-    else:
-        return False  # , None
-
-#get missing edges in case only three edges are detected
-#return [lonely edges, pair edges]
-
-def getMissingEdges(correctLines):
-    if checkSimilarAngle(correctLines[0][1], correctLines[1][1]):
-        return [correctLines[2], correctLines[0], correctLines[1]]
-    elif checkSimilarAngle(correctLines[0][1], correctLines[2][1]):
-        return [correctLines[1], correctLines[0], correctLines[2]]
-    else:
-        return correctLines
-
-
-#Resize Imgage
-
-width = img.shape[1]
-height = img.shape[0]
-ratio = 500 / width
-if width > 700:
-    ratio = 500 / width
-    resized = cv2.resize(img, None, fx = ratio, fy = ratio, interpolation = cv2.INTER_LINEAR)
-    img1 = resized
-else:
-    img1 = img
-width_0 = img1.shape[1]
-height_0 = img1.shape[0]
-
-#Pre-processing image
-
-img_padded = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_CONSTANT,value=[0,0,0])
-
-#Gray_img
-
-imgray = cv2.cvtColor(img_padded,cv2.COLOR_BGR2GRAY)
-
-#Candy edge detection
-
-edges = cv2.Canny(imgray, 70, 150)
-
-#Gaussian filter
-
-gauss = cv2.GaussianBlur(edges,(3,3),0)
-
-#Find contour
-
-contours, hierarchy = cv2.findContours(gauss,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
-
-#Find largest contour
-
-largest_cont = sorted(contours, key = cv2.contourArea, reverse = True)[1:2]
-
-#Binary image largest contour
-
-black = np.zeros((height_0, width_0), "uint8")
-
-draw = cv2.drawContours(black, largest_cont, -1, (255,255,255), 1)
-
-lines = cv2.HoughLines(black,1,np.pi/180, 50)
-blackHough = np.zeros((int(height * ratio), int(width * ratio)), "uint8")
-diagonal = math.sqrt(height ** 2 + width ** 2)
-lines0 = lines[:,0,:]
-for line in lines0:
-    rho, theta = line
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + math.ceil(diagonal) * (-b))
-    y1 = int(y0 + math.ceil(diagonal) * a)
-    x2 = int(x0 - math.ceil(diagonal) * (-b))
-    y2 = int(y0 - math.ceil(diagonal) * a)
-    cv2.line(blackHough, (x1, y1), (x2, y2), (255, 255, 255), 1)
-
-#Find 4 different lines in hough space
-correctLines = list()
-for line in lines:
-    if len(correctLines) == 4:
-        break
-    rho, theta = line[0]
-    isNew = True
-    numSimilar = 0
-    for l in correctLines:
-        correctTheta = l[1]
-        if checkSimilarAngle(theta, correctTheta):
-            numSimilar += 1
-            if numSimilar == 2:
-                isNew = False
-                break
-            if checkSimilarRho(line[0], l, img, rhoErr):
-                isNew = False
-                break
-    if isNew:
-        correctLines.append([rho, theta])
-    else:
-        continue
-
-for line in correctLines:
-        line[0] = line[0] / ratio
-numLines = len(correctLines)
-if numLines < 3:
-    raise Exception("Error: Receipt does not have enough edges to detect!")
-
-elif numLines == 3:
-    correctLines = getMissingEdges(correctLines)
-    rho, theta = correctLines[0]
-    intersections = getBoundaryIntersections(correctLines[1], img) + getBoundaryIntersections(correctLines[2],
-                                                                                                      img)
-    intersections.sort(key=getRho(theta))
-    if abs(getRho(theta)(intersections[1]) - rho) > abs(getRho(theta)(intersections[2]) - rho):
-        newLine = getParallel(correctLines[0], intersections[1])
-    else:
-        newLine = getParallel(correctLines[0], intersections[2])
-    correctLines.append(newLine)
-
-corners = list()
-for i in range(4):
-    for j in range(i + 1, 4):
-        if checkSimilarAngle(correctLines[i][1], correctLines[j][1]):
-                continue
-        try:
-            intersection = getIntersection(correctLines[i], correctLines[j])
-        except np.linalg.linalg.LinAlgError:
-            continue
-        else:
-            corners.append(intersection)
-if len(corners) != 4:
-    raise Exception('Error: Cannot get correct corners')
-topLeft, topRight, bottomRight, bottomLeft = getOrderPoints(np.array(corners, dtype="float32"))
-oldCorners = np.array([topLeft, topRight, bottomRight, bottomLeft], dtype="float32")
-# Compute new width and height
-newWidth = max(getDistance(topLeft, topRight), getDistance(bottomLeft, bottomRight))
-newHeight = max(getDistance(topLeft, bottomLeft), getDistance(topRight, bottomRight))
-# Compute 4 new corners
-newCorners = np.array([
-    [0, 0],
-    [newWidth - 1, 0],
-    [newWidth - 1, newHeight - 1],
-    [0, newHeight - 1]], dtype="float32")
-
-#Compute transformation matrix
-transMat = cv2.getPerspectiveTransform(oldCorners, newCorners)
-#Transform
-resultImage = cv2.warpPerspective(img, transMat, (int(newWidth), int(newHeight)))
-#resized_result = cv2.resize(resultImage, None, fx = ratio, fy = ratio, interpolation = cv2.INTER_LINEAR)
-test = cv2.cvtColor(resultImage,cv2.COLOR_BGR2RGB)
-print(pytesseract.image_to_string(test))
-cv2.imshow('Test', test)
-cv2.waitKey(0)
-
-
-
-
+    print("Ket qua dung: %i/%i" % (cnt_predict, len(list_files)))
+    print("Loi: %i" % fail_process)
+    print("Score = %.2f" % (10.*cnt_predict/len(list_files)))
+    print("Run time in: %.2f s" % run_time)
